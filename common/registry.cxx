2aeb2299ff1f91c8db02058924fa9fcddb55108b
Daniel Balchev <dbalchev@gmail.com>
2022-11-26 17:57:00 +0200

    Setup main code

--- common/registry.cxx
+++ common/registry.cxx
@@ -6,6 +6,7 @@ int Registry::run(int day, const char* input_filename) {
     auto solution_iterator = solutions.find(day);
     if (solution_iterator == solutions.end()) {
         std::cerr << "No solution for day " << day << std::endl;
+        return 1;
     }
     return solution_iterator->second->run(input_filename);
 }
--- main.cxx
+++ main.cxx
@@ -2,10 +2,12 @@
 
 #include <gflags/gflags.h>
 
+#include <advent_registry.h>
+
 DEFINE_int32(day, -1, "Which day to run");
+DEFINE_string(input, "", "Input file path");
 
 int main(int argc, char** argv) {
     gflags::ParseCommandLineFlags(&argc, &argv, true);
-    std::cout << "hey advent " << FLAGS_day << std::endl;
-    return 0;
+    return SOLUTION_REGISTRY.run(FLAGS_day, FLAGS_input.c_str());
 }
